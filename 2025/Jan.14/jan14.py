e3c2173d1ba516dbd5c73fd6c9efc7bf132a66fe
rklassen <richard.p.klassen@gmail.com>
2025-01-27 01:26:14 -0500

    Jan 14 apply screentone to greyscale images

--- 2025/Jan.14/jan14.py
+++ 2025/Jan.14/jan14.py
@@ -11,21 +11,6 @@ def simulated_ekg(period: float):
     s_scalar = 0.25 - 0.125 * sin ( 1.25 * phase - 1.0 )
     return 3.14 * q_scalar * r_wave * s_scalar
 
-# generate greyscale frames from the ekg of normalized t + u - v
-os.makedirs('.frames', exist_ok=True)
-for i in range(99):
-    greyscale_image = Image.new('RGB', (1080, 1080))
-    greyscale = greyscale_image.load()
-    for x in range(1080):
-        for y in range(1080):
-            period = float(i) / 100.0 + float(x - y) / 1079.0 / sqrt(2.0)
-            ekg = 0.5 + 0.5 * simulated_ekg(period)
-            color = int(254 * max(0.0, min(1.0, ekg)))
-            greyscale[x, y] = (color, color, color)
-    filename = f'.frames/grey_{i:03d}.png'
-    greyscale_image.save(filename)
-    print(f'Generated {filename}')
-
 # generate screentone image
 screentone_image = Image.new('L', (1080, 1080))
 screentone = screentone_image.load()
@@ -37,5 +22,21 @@ for x in range(1080):
         value = 0.5 + 0.5 * sin(2 * pi * u) * sin(2 * pi * v)
         color = int(254 * max(0.0, min(1.0, value)))
         screentone[x, y] = color
-screentone_image.save('screentone.png')
-print('Generated screentone.png')
+#screentone_image.save('screentone.png')
+print('Generated screentone values')
+
+# generate greyscale frames from the ekg of normalized t + u - v
+os.makedirs('.frames', exist_ok=True)
+for i in range(100):
+    bw_image = Image.new('RGB', (1080, 1080))
+    bw = bw_image.load()
+    for x in range(1080):
+        for y in range(1080):
+            period = float(i) / 100.0 + float(x - y) / 1079.0 / sqrt(2.0)
+            ekg = 0.5 + 0.5 * simulated_ekg(period)
+            grey_value = int(254 * max(0.0, min(1.0, ekg)))
+            color = 0 if grey_value < screentone[x, y] else 255
+            bw[x, y] = (color, color, color)
+    filename = f'.frames/bw_{i:03d}.png'
+    bw_image.save(filename)
+    print(f'Generated {filename}')
