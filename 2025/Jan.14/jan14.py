a57eb86529778ff2e857d209f9c97c5c8f9290fa
rklassen <richard.p.klassen@gmail.com>
2025-01-27 01:16:47 -0500

    Jan 14 bw screentone

--- 2025/Jan.14/jan14.py
+++ 2025/Jan.14/jan14.py
@@ -14,14 +14,28 @@ def simulated_ekg(period: float):
 # generate greyscale frames from the ekg of normalized t + u - v
 os.makedirs('.frames', exist_ok=True)
 for i in range(99):
-    img = Image.new('RGB', (1080, 1080))
-    pixels = img.load()
+    greyscale_image = Image.new('RGB', (1080, 1080))
+    greyscale = greyscale_image.load()
     for x in range(1080):
         for y in range(1080):
             period = float(i) / 100.0 + float(x - y) / 1079.0 / sqrt(2.0)
             ekg = 0.5 + 0.5 * simulated_ekg(period)
             color = int(254 * max(0.0, min(1.0, ekg)))
-            pixels[x, y] = (color, color, color)
+            greyscale[x, y] = (color, color, color)
     filename = f'.frames/grey_{i:03d}.png'
-    img.save(filename)
+    greyscale_image.save(filename)
     print(f'Generated {filename}')
+
+# generate screentone image
+screentone_image = Image.new('L', (1080, 1080))
+screentone = screentone_image.load()
+screen_dpi = 16.0
+for x in range(1080):
+    for y in range(1080):
+        u = float(x) * screen_dpi / 1079.0
+        v = float(y) * screen_dpi / 1079.0
+        value = 0.5 + 0.5 * sin(2 * pi * u) * sin(2 * pi * v)
+        color = int(254 * max(0.0, min(1.0, value)))
+        screentone[x, y] = color
+screentone_image.save('screentone.png')
+print('Generated screentone.png')
