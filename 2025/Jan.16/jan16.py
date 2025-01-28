f01da9f457e05267b76056c6da48dacd03aa1dc0
rklassen <richard.p.klassen@gmail.com>
2025-01-27 23:58:33 -0500

    Jan 16 palette categories

--- 2025/Jan.16/jan16.py
+++ 2025/Jan.16/jan16.py
@@ -1,10 +1,30 @@
 from PIL import Image, ImageDraw
 from math import sin, cos
 import random
+# region pseudostruct
+class ColorCategory:
+    def __init__(self, s_min, s_max, l_min, l_max):
+        self.s_min = s_min
+        self.s_max = s_max
+        self.l_min = l_min
+        self.l_max = l_max
+# endregion
+
+# region pseudoconst
 PALETTE_COLOR_MIN = 214
 PALETTE_COLOR_MAX = 218
 PALETTE_RADIUS = 0.025
 IMG_WIDTH = 1024
+# thanks to https://medium.com/@vermaayushi713
+# https://medium.com/design-bootcamp/understanding-color-theory-part-3-f3b7a61c8790
+earth  = ColorCategory(0.36, 0.41, 0.36, 0.77)
+greys  = ColorCategory(0.00, 0.04, 0.00, 1.00)
+jewel  = ColorCategory(0.73, 0.83, 0.56, 0.76)
+muted  = ColorCategory(0.01, 0.10, 0.70, 0.99)
+neon   = ColorCategory(0.63, 1.00, 0.82, 1.00)
+pastel = ColorCategory(0.14, 0.21, 0.89, 0.96)
+CATEGORIES = [earth, greys, jewel, muted, neon, pastel]
+# endregion
 
 # region init
 mask = Image.open('mask.png')
@@ -23,7 +43,7 @@ def is_inside_mask(u, v, radius):  # radius in uv space
         theta += 6.28 / 7.0
     return True
 
-def hsl_to_rgb(h, s, l):  # h in [0, 6.28], s, l in [0, 1]
+def hsl_to_rgb(h, s, l):  # hue in radians; s, l in [0, 1]
     c = (1 - abs(2 * l - 1)) * s
     x = c * (1 - abs((h / 1.047) % 2 - 1))
     m = l - c / 2
@@ -33,14 +53,15 @@ def hsl_to_rgb(h, s, l):  # h in [0, 6.28], s, l in [0, 1]
 #endregion
 
 # region draw palette
-grid = PALETTE_RADIUS / 3
-jitter = PALETTE_RADIUS / 2
+grid = PALETTE_RADIUS / 2.25
+jitter = PALETTE_RADIUS / 8
 for v in range(0, int(v_max / grid)):
     for u in range(0, int(u_max / grid)):
-        uj = u * grid + random.uniform(-jitter, jitter)
-        vj = v * grid + random.uniform(-jitter, jitter)
+        uj, vj = u * grid, v * grid
         if not is_inside_mask(uj, vj, PALETTE_RADIUS):
             continue
+        uj += random.uniform(-jitter, jitter)
+        vj += random.uniform(-jitter, jitter)
         x, y = (int(uj * IMG_WIDTH), int(vj * IMG_WIDTH))
         color = hsl_to_rgb(random.uniform(0, 1), 0.04, random.uniform(0.82, 0.86))
         radius = int(PALETTE_RADIUS * IMG_WIDTH)
