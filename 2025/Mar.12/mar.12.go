91616a0f1856569f47171bd9004f759002fd6434
rklassen <richard.p.klassen@gmail.com>
2025-05-06 23:27:09 -0400

    go prune

--- 2025/Mar.12/mar.12.go
+++ 2025/Mar.12/mar.12.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"image/png"
-
-	// "log"
 	"os"
 
 	"github.com/chai2010/webp"
@@ -15,9 +13,10 @@ func main() {
 	var buf bytes.Buffer
 
 	data, _ := os.ReadFile("./coffee_sq.png")
-	img, _ := png.Decode(bytes.NewReader(data))
-	_ = webp.Encode(&buf, img, &webp.Options{Lossless: false})
-	_ = os.WriteFile("./coffee_sq.webp", buf.Bytes(), 0666)
+	img, other_value := png.Decode(bytes.NewReader(data))
+	fmt.Println("Other value:", other_value)
+	webp.Encode(&buf, img, &webp.Options{Lossless: false})
+	os.WriteFile("./coffee_sq.webp", buf.Bytes(), 0666)
 	fmt.Println("Save ./coffee_sq.webp ok")
 }
 
